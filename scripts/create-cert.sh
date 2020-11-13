4c1e66e09a645ee3e979282d9dc45012a94a31e5
Sergey Sova <mail@sergeysova.com>
2020-11-13 21:51:27 +0300

    refactor: split to internal and public api, manual prod deploy

--- scripts/create-cert.sh
+++ scripts/create-cert.sh
@@ -2,7 +2,7 @@
 
 openssl req -x509 -out ./tls/accesso.crt -keyout ./tls/accesso.pem \
   -newkey rsa:2048 -nodes -sha256 \
-  -subj '/CN=auth.local' -extensions EXT -config <( \
-   printf "[dn]\nCN=auth.local\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:auth.local\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+  -subj '/CN=accesso.local' -extensions EXT -config <( \
+   printf "[dn]\nCN=accesso.local\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:accesso.local\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 
 openssl dhparam -out ./tls/dhparam.pem 2048
